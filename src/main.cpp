40851ae5621fb4d270d4b2129008f04f2657e1f9
Jaysurya85 <jaysuryaray1998@gmail.com>
2026-05-18 00:03:58 -0400

    Got details of table name and column names from sql

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
+#include "SQLParser.h"
 #include <iostream>
 
 int main() {
+  std::string sql =
+      "CREATE TABLE orders (id INT, user_id INT, amount DECIMAL(10, 2));";
+
+  hsql::SQLParserResult result;
+  hsql::SQLParser::parse(sql, &result);
+  std::vector<hsql::SQLStatement *> statements = result.getStatements();
+
   std::cout << "Welcome to Schemaforge" << '\n';
+
+  for (const auto &stmt : statements) {
+    if (stmt->isType(hsql::StatementType::kStmtCreate)) {
+      std::cout << "Parsed a CREATE statement.\n";
+      auto *create_stmt = static_cast<hsql::CreateStatement *>(stmt);
+      std::cout << "Table name: " << create_stmt->tableName << '\n';
+      std::cout << "Columns are:\n";
+      for (const auto &col : *create_stmt->columns) {
+        std::cout << "  - " << col->name << " (" << col->type << ")\n";
+      }
+    }
+  }
   return 0;
 }
